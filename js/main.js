54fe12d90a7b87b96c017db94c74fa2088fbe556
Rovalda <174670550+RovaldaVC@users.noreply.github.com>
2025-07-04 17:41:40 +0330

    Third Structure
    Correct way of using Javascript.
    the code wasn't working well at the second structure because task would be deleted after a new task was added.

--- js/main.js
+++ js/main.js
@@ -3,28 +3,27 @@ function newActionPage(){
 }
 
 function addAction(){
+
     const time = document.querySelector("#time").value;
     const action = document.querySelector("#action").value;
-    
-    localStorage.setItem("newTime", time)
-    localStorage.setItem("newAction", action)
 
+    let tasks = JSON.parse(localStorage.getItem("tasks")) ||[];
+    tasks.push({time, action})
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    
     window.location.href = "index.html"
 }
 
 function contentLoaded(){
-    const time = localStorage.getItem("newTime")
-    const action = localStorage.getItem("newAction")
-    
-    if (time && action){
-        const details = document.querySelector(".details");
-        details.innerHTML += `
-        <li class=item1>${time}</li>
-        <li class=item2>${action}</li>
-        `
-        localStorage.removeItem("newTime")
-        localStorage.removeItem("newAction")
-    }
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const details = document.querySelector(".details");
+
+    tasks.forEach(task => {
+        details.innerHTML +=`
+        <li class='item1'> ${task.time} </li>
+        <li class='item2'> ${task.action} </li>
+        `;
+    })
 }
 
 window.addEventListener("DOMContentLoaded", contentLoaded())
